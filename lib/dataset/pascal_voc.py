654472a0cbfb7dc22061e9efd4d495a44e4f643a
Zheng Zhang <zhez@microsoft.com>
2017-05-15 13:54:51 +0800

    removing dependency of hkl, and modify the cfg

--- lib/dataset/pascal_voc.py
+++ lib/dataset/pascal_voc.py
@@ -2,7 +2,7 @@
 # Deformable Convolutional Networks
 # Copyright (c) 2017 Microsoft
 # Licensed under The Apache-2.0 License [see LICENSE for details]
-# Modified by Haozhi Qi, Zheng Zhang, from py-faster-rcnn (https://github.com/rbgirshick/py-faster-rcnn)
+# Modified by Haozhi Qi, from py-faster-rcnn (https://github.com/rbgirshick/py-faster-rcnn)
 # --------------------------------------------------------
 
 """
@@ -22,7 +22,6 @@ import PIL
 from imdb import IMDB
 from pascal_voc_eval import voc_eval, voc_eval_sds
 from ds_utils import unique_boxes, filter_small_boxes
-import hickle as hkl
 
 class PascalVOC(IMDB):
     def __init__(self, image_set, root_path, devkit_path, result_path=None, mask_size=-1, binary_thresh=None):
@@ -80,27 +79,6 @@ class PascalVOC(IMDB):
         assert os.path.exists(image_file), 'Path does not exist: {}'.format(image_file)
         return image_file
 
-    def mask_path_from_index(self, index, gt_mask):
-        """
-        given image index, cache high resolution mask and return full path of masks
-        :param index: index of a specific image
-        :return: full path of this mask
-        """
-        if self.image_set == 'val':
-            return []
-        cache_file = os.path.join(self.cache_path, 'VOCMask')
-        if not os.path.exists(cache_file):
-            os.makedirs(cache_file)
-        # instance level segmentation
-        gt_mask_file = os.path.join(cache_file, index + '.hkl')
-        if not os.path.exists(gt_mask_file):
-            hkl.dump(gt_mask.astype('bool'), gt_mask_file, mode='w', compression='gzip')
-        # cache flip gt_masks
-        gt_mask_flip_file = os.path.join(cache_file, index + '_flip.hkl')
-        if not os.path.exists(gt_mask_flip_file):
-            hkl.dump(gt_mask[:, :, ::-1].astype('bool'), gt_mask_flip_file, mode='w', compression='gzip')
-        return gt_mask_file
-
     def segmentation_path_from_index(self, index):
         """
         given image index, find out the full path of segmentation class
@@ -129,24 +107,6 @@ class PascalVOC(IMDB):
         print 'wrote gt roidb to {}'.format(cache_file)
 
         return gt_roidb
-
-    def gt_sdsdb(self):
-        """
-        :return:
-        """
-        cache_file = os.path.join(self.cache_path, self.name + '_gt_sdsdb.pkl')
-        if os.path.exists(cache_file):
-            with open(cache_file, 'rb') as fid:
-                sdsdb = cPickle.load(fid)
-            print '{} gt sdsdb loaded from {}'.format(self.name, cache_file)
-            return sdsdb
-        # for internal useage
-        gt_sdsdb = [self.load_pascal_sds_annotation(index) for index in self.image_set_index]
-        with open(cache_file, 'wb') as fid:
-            cPickle.dump(gt_sdsdb, fid, cPickle.HIGHEST_PROTOCOL)
-        # for future release usage
-        # need to implement load sbd data
-        return gt_sdsdb
 
     def gt_segdb(self):
         """
@@ -265,68 +225,6 @@ class PascalVOC(IMDB):
 
         return roidb
 
-    def load_pascal_sds_annotation(self, index):
-        print index
-        sds_rec = dict()
-        sds_rec['image'] = self.image_path_from_index(index)
-        size = cv2.imread(sds_rec['image']).shape
-        sds_rec['height'] = size[0]
-        sds_rec['width'] = size[1]
-        # class level segmentation
-        seg_cls_name = os.path.join(self.data_path, 'SegmentationClass', index + '.png')
-        seg_cls_data = PIL.Image.open(seg_cls_name)
-        seg_cls_data = np.array(seg_cls_data.getdata(), np.uint8).reshape(seg_cls_data.size[1], seg_cls_data.size[0])
-        # instance level segmentation
-        seg_obj_name = os.path.join(self.data_path, 'SegmentationObject', index + '.png')
-        seg_obj_data = PIL.Image.open(seg_obj_name)
-        seg_obj_data = np.array(seg_obj_data.getdata(), np.uint8).reshape(seg_obj_data.size[1], seg_obj_data.size[0])
-        # check unique instance
-        unique_inst = np.unique(seg_obj_data)
-        bg_inds = np.where(unique_inst == 0)[0]
-        unique_inst = np.delete(unique_inst, bg_inds)
-        border_inds = np.where(unique_inst == 255)[0]
-        unique_inst = np.delete(unique_inst, border_inds)
-
-        num_objs = len(unique_inst)
-        boxes = np.zeros((num_objs, 4), dtype=np.uint16)
-        gt_classes = np.zeros(num_objs, dtype=np.int32)
-        overlaps = np.zeros((num_objs, self.num_classes), dtype=np.float32)
-        # TODO: figure out a way to pass mask size
-        gt_masks = np.zeros((num_objs, size[0], size[1]))
-
-        for idx, inst_id in enumerate(unique_inst):
-            [r, c] = np.where(seg_obj_data == inst_id)
-            x1 = np.min(c)
-            x2 = np.max(c)
-            y1 = np.min(r)
-            y2 = np.max(r)
-            cur_gt_mask = (seg_obj_data == inst_id)
-            cur_gt_mask_cls = seg_cls_data[cur_gt_mask]
-            #roi_mask = (seg_obj_data[y1:y2+1, x1:x2+1] == inst_id)
-            #roi_mask_cls = seg_cls_data[y1:y2+1, x1:x2+1]
-            #roi_mask_cls = roi_mask_cls[roi_mask]
-            assert np.unique(cur_gt_mask_cls).shape[0] == 1
-            cur_inst_cls = np.unique(cur_gt_mask_cls)[0]
-            # resize mask to fixed size and convert to boolean
-            # roi_mask = cv2.resize(roi_mask.astype(np.float32), (self.mask_size, self.mask_size))
-
-            boxes[idx, :] = [x1, y1, x2, y2]
-            gt_classes[idx] = cur_inst_cls
-            gt_masks[idx, :, :] = cur_gt_mask
-            overlaps[idx, cur_inst_cls] = 1.0
-
-        sds_rec.update({
-            'boxes': boxes,
-            'gt_classes': gt_classes,
-            'gt_overlaps': overlaps,
-            'max_classes': overlaps.argmax(axis=1),
-            'max_overlaps': overlaps.max(axis=1),
-
-            'cache_seg_inst': self.mask_path_from_index(index, gt_masks),
-            'flipped': False
-        })
-        return sds_rec
-
     def load_pascal_segmentation_annotation(self, index):
         """
         for a given index, load image and bounding boxes info from XML file
@@ -364,11 +262,6 @@ class PascalVOC(IMDB):
 
         self.write_pascal_results(detections)
         info = self.do_python_eval()
-        return info
-
-    def evaluate_sds(self, all_boxes, all_masks):
-        self._write_voc_sds_results_file(all_boxes, all_masks)
-        info = self._py_evaluate_mask()
         return info
 
     def evaluate_segmentations(self, pred_segmentations):
@@ -411,48 +304,6 @@ class PascalVOC(IMDB):
             segmentation_result.putpalette(pallete)
             segmentation_result.save(os.path.join(result_dir, '%s.png'%(index)))
 
-    def _write_voc_sds_results_file(self, all_boxes, all_masks):
-        """
-        Write results as a pkl file, note this is different from
-        detection task since it's difficult to write masks to txt
-        """
-        # make all these folders for results
-        result_dir = os.path.join(self.result_path, 'results')
-        if not os.path.exists(result_dir):
-            os.mkdir(result_dir)
-        # Always reformat result in case of sometimes masks are not
-        # binary or is in shape (n, sz*sz) instead of (n, sz, sz)
-        all_boxes, all_masks = self._reformat_result(all_boxes, all_masks)
-        for cls_inds, cls in enumerate(self.classes):
-            if cls == '__background__':
-                continue
-            print 'Writing {} VOC results file'.format(cls)
-            filename = os.path.join(result_dir, cls + '_det.pkl')
-            print filename
-            with open(filename, 'wb') as f:
-                cPickle.dump(all_boxes[cls_inds], f, cPickle.HIGHEST_PROTOCOL)
-            filename = os.path.join(result_dir, cls + '_seg.pkl')
-            with open(filename, 'wb') as f:
-                cPickle.dump(all_masks[cls_inds], f, cPickle.HIGHEST_PROTOCOL)
-
-    def _reformat_result(self, boxes, masks):
-        num_images = self.num_images
-        num_class = len(self.classes)
-        reformat_masks = [[[] for _ in xrange(num_images)]
-                          for _ in xrange(num_class)]
-        for cls_inds in xrange(1, num_class):
-            for img_inds in xrange(num_images):
-                if len(masks[cls_inds][img_inds]) == 0:
-                    continue
-                num_inst = masks[cls_inds][img_inds].shape[0]
-                #print num_inst
-                #print masks[cls_inds][img_inds].shape
-                reformat_masks[cls_inds][img_inds] = masks[cls_inds][img_inds]\
-                    .reshape(num_inst, self.mask_size, self.mask_size)
-                # reformat_masks[cls_inds][img_inds] = reformat_masks[cls_inds][img_inds] >= 0.4
-        all_masks = reformat_masks
-        return boxes, all_masks
-
     def get_pallete(self, num_cls):
         """
         this function is to get the colormap for visualizing the segmentation mask
@@ -494,53 +345,6 @@ class PascalVOC(IMDB):
                     confusion_matrix[i_label, i_pred_label] = label_count[cur_index]
 
         return confusion_matrix
-
-    def _py_evaluate_mask(self):
-        info_str = ''
-        gt_dir = self.data_path
-        imageset_file = os.path.join(self.data_path, 'ImageSets', 'Main', self.image_set + '.txt')
-        cache_dir = os.path.join(self.devkit_path, 'annotations_cache')
-        output_dir = os.path.join(self.result_path, 'results')
-        aps = []
-        # define this as true according to SDS's evaluation protocol
-        use_07_metric = True
-        print 'VOC07 metric? ' + ('Yes' if use_07_metric else 'No')
-        info_str += 'VOC07 metric? ' + ('Y' if use_07_metric else 'No')
-        info_str += '\n'
-        if not os.path.isdir(output_dir):
-            os.mkdir(output_dir)
-        print '~~~~~~ Evaluation use min overlap = 0.5 ~~~~~~'
-        info_str += '~~~~~~ Evaluation use min overlap = 0.5 ~~~~~~'
-        info_str += '\n'
-        for i, cls in enumerate(self.classes):
-            if cls == '__background__':
-                continue
-            det_filename = os.path.join(output_dir, cls + '_det.pkl')
-            seg_filename = os.path.join(output_dir, cls + '_seg.pkl')
-            ap = voc_eval_sds(det_filename, seg_filename, gt_dir,
-                              imageset_file, cls, cache_dir, self.classes, self.mask_size, self.binary_thresh, ov_thresh=0.5)
-            aps += [ap]
-            print('AP for {} = {:.2f}'.format(cls, ap*100))
-            info_str += 'AP for {} = {:.2f}\n'.format(cls, ap*100)
-        print('Mean AP@0.5 = {:.2f}'.format(np.mean(aps)*100))
-        info_str += 'Mean AP@0.5 = {:.2f}\n'.format(np.mean(aps)*100)
-        print '~~~~~~ Evaluation use min overlap = 0.7 ~~~~~~'
-        info_str += '~~~~~~ Evaluation use min overlap = 0.7 ~~~~~~\n'
-        aps = []
-        for i, cls in enumerate(self.classes):
-            if cls == '__background__':
-                continue
-            det_filename = os.path.join(output_dir, cls + '_det.pkl')
-            seg_filename = os.path.join(output_dir, cls + '_seg.pkl')
-            ap = voc_eval_sds(det_filename, seg_filename, gt_dir,
-                              imageset_file, cls, cache_dir, self.classes, self.mask_size, self.binary_thresh, ov_thresh=0.7)
-            aps += [ap]
-            print('AP for {} = {:.2f}'.format(cls, ap*100))
-            info_str += 'AP for {} = {:.2f}\n'.format(cls, ap*100)
-        print('Mean AP@0.7 = {:.2f}'.format(np.mean(aps)*100))
-        info_str += 'Mean AP@0.7 = {:.2f}\n'.format(np.mean(aps)*100)
-
-        return info_str
 
     def _py_evaluate_segmentation(self):
         """
