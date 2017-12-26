7703085dc4ebd19daf8c513820cf625a57440f71
Oh233 <macshqi@gmail.com>
2017-12-26 20:37:20 +0800

    Add feature pyramid network (FPN) and deformable FPN support

--- lib/nms/nms.py
+++ lib/nms/nms.py
@@ -3,9 +3,16 @@ import numpy as np
 from cpu_nms import cpu_nms
 from gpu_nms import gpu_nms
 
+
 def py_nms_wrapper(thresh):
     def _nms(dets):
         return nms(dets, thresh)
+    return _nms
+
+
+def py_softnms_wrapper(thresh, max_dets=-1):
+    def _nms(dets):
+        return soft_nms(dets, thresh, max_dets)
     return _nms
 
 
@@ -59,3 +66,62 @@ def nms(dets, thresh):
         order = order[inds + 1]
 
     return keep
+
+
+def rescore(overlap, scores, thresh, type='gaussian'):
+    assert overlap.shape[0] == scores.shape[0]
+    if type == 'linear':
+        inds = np.where(overlap >= thresh)[0]
+        scores[inds] = scores[inds] * (1 - overlap[inds])
+    else:
+        scores = scores * np.exp(- overlap**2 / thresh)
+
+    return scores
+
+
+def soft_nms(dets, thresh, max_dets):
+    if dets.shape[0] == 0:
+        return np.zeros((0, 5))
+
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    scores = dets[:, 4]
+
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    order = scores.argsort()[::-1]
+    scores = scores[order]
+
+    if max_dets == -1:
+        max_dets = order.size
+
+    keep = np.zeros(max_dets, dtype=np.intp)
+    keep_cnt = 0
+
+    while order.size > 0 and keep_cnt < max_dets:
+        i = order[0]
+        dets[i, 4] = scores[0]
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+        order = order[1:]
+        scores = rescore(ovr, scores[1:], thresh)
+
+        tmp = scores.argsort()[::-1]
+        order = order[tmp]
+        scores = scores[tmp]
+
+        keep[keep_cnt] = i
+        keep_cnt += 1
+
+    keep = keep[:keep_cnt]
+    dets = dets[keep, :]
+    return dets
